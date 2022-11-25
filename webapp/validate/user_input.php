c772cc1b3f50cc792b7be1460e285bc8bf0d37a4
Vo Huy <huyvntse161762@fpt.edu.vn>
2022-11-26 00:20:47 +0700

    comment

--- webapp/validate/user_input.php
+++ webapp/validate/user_input.php
@@ -1,12 +1,12 @@
 <?php
-ini_set("display_errors", "1");
-include_once('../frameworks/library/HTMLPurifier.auto.php');
+// ini_set("display_errors", "1");
+// include_once('../frameworks/library/HTMLPurifier.auto.php');
 
-$config = HTMLPurifier_Config::createDefault();
-function purify($dirty)
-{
-    global $config;
-    $purifier = new HTMLPurifier($config);
-    $pureHTML = htmlspecialchars($purifier->purify($dirty));
-    return $pureHTML;
-}
+// $config = HTMLPurifier_Config::createDefault();
+// function purify($dirty)
+// {
+//     global $config;
+//     $purifier = new HTMLPurifier($config);
+//     $pureHTML = htmlspecialchars($purifier->purify($dirty));
+//     return $pureHTML;
+// }
